8179286feab661abc8d06ee0415e4e916adbc3e1
wojciech.palubski <wojciech.palubski@asseco.pl>
2018-01-31 16:44:06 +0100

    przechwycenie wyjątku całego splita

--- src/main/java/pl/wpe/SplitAgregator.java
+++ src/main/java/pl/wpe/SplitAgregator.java
@@ -11,7 +11,7 @@ public class SplitAgregator implements AggregationStrategy {
             }
             String body = newExchange.getIn().getBody(String.class);
             String existing = oldExchange.getIn().getBody(String.class);
-            System.out.println("Agregator Do starego: "+existing+", dodaje nowe: "+body);
+            System.out.println("Agregator, do starego: "+existing+", dodaje nowe: "+body);
             oldExchange.getIn().setBody(existing + "\n" + body);
             return oldExchange;
         }
